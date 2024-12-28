9366f9224667d0eb35e54f890086077f54bec96c
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2024-12-28 07:23:13 +0000

    design: setup next app layout and theme

---
+++ web/src/components/layout/Header.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Header() {
+    return (
+        <header className="fixed top-0 w-full inset-x-0 bg-white z-50">
+            <nav className="container flex items-center justify-between py-4">
+                <Image src="/images/logo.svg" alt="Azure Task" width={120} height={40} />
+
+                <ul className="flex items-center space-x-4">
+                    <li className="btn btn-outline">Login</li>
+                    <li className="btn btn-primary">Get Started</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -7,12 +7,43 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+      }
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          50: '#ebf5ff',
+          100: '#d6eaff',
+          200: '#add6ff',
+          300: '#84c1ff',
+          400: '#5badff',
+          500: '#2b96ff',
+          600: '#007bff', // Primary DEFAULT
+          700: '#0066cc',
+          800: '#004d99',
+          900: '#003366',
+          DEFAULT: '#007bff',
+        },
+        secondary: {
+          50: '#e6f9fb',
+          100: '#ccf3f6',
+          200: '#99e6ec',
+          300: '#66dae3',
+          400: '#33cdd9',
+          500: '#00c1d4', // Secondary DEFAULT
+          600: '#00a7b9',
+          700: '#008d9e',
+          800: '#007482',
+          900: '#005a67',
+          DEFAULT: '#00c1d4',
+        },
       },
-    },
+    }
+
   },
   plugins: [],
 } satisfies Config;
